a12281bd3af387e993ff52118fdd38a4f837d0d3
OrtegaDiana <demonofdiana@gmail.com>
2020-01-24 20:00:34 +0100

    Escena hasta punto 15

---
+++ Assets/Diana/Scripts/Navigation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Navigation : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] UnityEngine.AI.NavMeshAgent agent;
+    [SerializeField] float updateDelay = .3f;
+    private void Reset()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        // agent.SetDestination(target.position);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(target.position);
+    }
+}
---
+++ Assets/Diana/Scripts/TranslateAndRotate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TranslateAndRotate : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] UnityEngine.AI.NavMeshAgent agent;
+    [SerializeField] float updateDelay = .3f;
+    private void Reset()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("FollowTarget", 0f, updateDelay);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(target.position);
+    }
+}
